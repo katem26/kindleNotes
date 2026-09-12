ac5fc39126ba0f2bd96b346a980d6d7387f212cc
Katelyn M <255952759+katem26@users.noreply.github.com>
2026-09-12 20:08:55 +0100

    Fixed bug where line breaks would make notes uneditable

--- kindleNotes/script.js
+++ kindleNotes/script.js
@@ -1,65 +1,66 @@
+var notesContainer, newNoteButton;
+
+function showNotes(){
+    notesContainer.innerHTML = localStorage.getItem("notes") || "";
+}
+
+function updateNotes(){
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+function newNote()
+{
+    // create note
+    var note = document.createElement("div");
+    note.className = "note";
+
+    // create text box
+    var inputBox = document.createElement("div");
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable", "true");
+
+    // create delete button
+    var deleteButton = document.createElement("button");
+    deleteButton.className = "delete-button";
+    deleteButton.innerHTML = "Delete";
+
+    // add to container and save
+    note.appendChild(inputBox)
+    note.appendChild(deleteButton);
+    notesContainer.insertBefore(note, notesContainer.firstChild); // new notes are added to top
+    updateNotes();
+}
+
+
 window.onload = function()
 {
-    var notesContainer = document.querySelector("#notes-container");
-    var newNoteButton = document.querySelector("#new-note-button");
-
     // set header
     function setHeader()
-    {
+     {
         kindle.messaging.sendMessage("com.lab126.chromebar", "configureChrome",
             {
                 "appId": "com.katem26.kindlenotes",
                 "topNavBar":
-                    {
-                        "template": "title",
-                        "title": "Kindle Notes"
-                    }
+                {
+                    "template": "title",
+                    "title": "Kindle Notes"
+                }
             }
         );
     }
 
-    window.kindle.appmgr.ongo = function (ctx) {
+    window.kindle.appmgr.ongo = function (ctx)
+    {
         setHeader();
     }
 
-    // load previous notes
-    function showNotes(){
-        notesContainer.innerHTML = localStorage.getItem("notes") || "";
-    }
+    notesContainer = document.querySelector("#notes-container");
+    newNoteButton = document.querySelector("#new-note-button");
+
     showNotes();
+    newNoteButton.addEventListener("click", newNote);
 
-    // store edits
-    function updateNotes(){
-        localStorage.setItem("notes", notesContainer.innerHTML);
-    }
-
-
-    // add note (button event)
-    newNoteButton.addEventListener("click", function()
-        {
-            // create note
-            var note = document.createElement("div");
-            note.className = "note";
-
-            // create text box
-            var inputBox = document.createElement("p");
-            inputBox.className = "input-box";
-            inputBox.setAttribute("contenteditable", "true");
-
-            // create delete button
-            var deleteButton = document.createElement("button");
-            deleteButton.className = "delete-button";
-            deleteButton.innerHTML = "Delete";
-
-            // add to container and save
-            note.appendChild(inputBox)
-            note.appendChild(deleteButton);
-            notesContainer.insertBefore(note, notesContainer.firstChild); // new notes are added to top
-            updateNotes();
-        }
-    );
-
-    // delete button event
+    // event handler for delete buttons
     notesContainer.addEventListener("click", function(event)
         {
             if (event.target.className === "delete-button")
@@ -72,12 +73,7 @@ window.onload = function()
     );
 
     // save while typing
-    notesContainer.addEventListener("input", function()
-        {
-            updateNotes();
-        }
-    );
-
+    notesContainer.addEventListener("input", updateNotes);
 
 }; // window.onload
 
